bf4229f5f45400d416805c59938fc88b0f023407
SeokRae <kslbsh@gmail.com>
2024-11-17 22:01:03 +0900

    Initial commit

---
+++ src/main/java/org/example/slack/IntegrationSlackAdapterApplication.java
@@ -0,0 +1,13 @@
+package org.example.slack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationSlackAdapterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IntegrationSlackAdapterApplication.class, args);
+  }
+
+}
